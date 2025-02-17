0f536d10a9619e42b842c8a9b6bafe962d1c50bf
Stelios Tsampas <stelios.tsampas@fau.de>
2025-02-17 01:02:27 +0100

    Correct directory structure

---
+++ lectures/src/main/java/cp/Week8/ParallelExample.java
@@ -0,0 +1,30 @@
+package cp.Week8;
+
+// This is pretty much parallel.
+
+class ParTask1 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 7; i++) {
+           try { Thread.sleep((long)(Math.random() * 500)); } catch (InterruptedException ignored) {} // Simulate random delay
+        }
+        System.out.println("Done!");
+    }
+}
+
+class ParTask2 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 7; i++) {
+            try { Thread.sleep((long)(Math.random() * 500)); } catch (InterruptedException ignored) {} // Simulate random delay
+        }
+        System.out.println("Done!");
+    }
+}
+
+public class ParallelExample {
+    public static void main(String[] args) {
+        ParTask1 t1 = new ParTask1();
+        ParTask2 t2 = new ParTask2();
+        t1.start();
+        t2.start();
+    }
+}
