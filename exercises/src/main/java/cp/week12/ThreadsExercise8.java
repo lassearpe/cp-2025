b4f7391d1b05cd430b45d50d7fe0574083f90744
Lasse Arpe Kristensen <lakri18@student.sdu.dk>
2025-03-24 12:00:27 +0100

    changes week12

--- exercises/src/main/java/cp/week12/ThreadsExercise8.java
+++ exercises/src/main/java/cp/week12/ThreadsExercise8.java
@@ -11,11 +11,7 @@ public class ThreadsExercise8
 	- Reason about the pros and cons of the two concurrency strategies
 	  (write them down).
 
-<<<<<<< HEAD
 	  	- Which one is faster? Can you think and implement a test routine that 
-=======
-	- Which one is faster? Can you think and implement a test routine that 
->>>>>>> 7655dfbc4b4324da71dfef69b5934e5d0cc71e12
 	  dependably measures and compares the execution time of the two 
 	  implementations?
 	*/
